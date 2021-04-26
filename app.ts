d79e7d3060a02624423b8a8748c2609bed247525
Brian Francis <13bfrancis@gmail.com>
2021-04-26 17:58:15 -0400

    initial project

---
+++ app.ts
@@ -0,0 +1,31 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    helloWorld: test!
+  }
+  type test {
+    testMessage: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    helloWorld: () => {
+      return {
+        testMessage: "Hello World!",
+      };
+    },
+  },
+};
+
+(async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  try {
+    const app = await server.listen(process.env.PORT || 4000);
+    console.log(`Server ready on ${app.url}`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
